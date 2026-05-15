0f7decbe5962ef4de9ff812dc813b6ead14e5538
ohjinsol77 <47802121+ohjinsol77@users.noreply.github.com>
2026-05-15 15:06:14 +0900

    Remove repository contents

--- src/kis/models.rs
+++
@@ -1,8 +0,0 @@
-use serde::{Deserialize, Serialize};
-
-#[derive(Debug, Clone, Serialize, Deserialize)]
-pub struct KisApiErrorBody {
-    pub rt_cd: Option<String>,
-    pub msg_cd: Option<String>,
-    pub msg1: Option<String>,
-}
